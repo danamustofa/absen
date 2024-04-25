10da41de1b83fe93f1cca4aac16b94c4d738b88d
Dana Mustofa <danamustofa13@gmail.com>
2024-04-25 20:34:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
 use App\Http\Controllers\StudentController;
 
 /*
@@ -14,19 +13,6 @@ use App\Http\Controllers\StudentController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/bel', function () {
-    return view('belajar');
-});
-
-Route::get('/page',[PageController::class,'home']);
-Route::get('/page/about',[PageController::class,'about']);
-Route::get('/page/contact',[PageController::class,'contact']);
-
 Route::get('student', [StudentController::class,'index']);
 Route::get('student/add', [StudentController::class,'add']);
 Route::post('student/addsave', [StudentController::class,'addsave']);
